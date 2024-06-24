a882ddfba3152d8e0dace7613857bb333a5e4276
sallebruno <9392@holbertonstudents.com>
2024-06-24 12:58:31 +0000

    program 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - cuenta
+ *
+ * @s: almacena
+ *
+ * Return: retona la cantidad de los caracteres de la cadena
+ */
+int _strlen(char *s)
+{
+	int lent;
+
+	for (lent = 0; s[lent] != '\0'; lent++)
+	{
+	}
+	return (lent);
+}
