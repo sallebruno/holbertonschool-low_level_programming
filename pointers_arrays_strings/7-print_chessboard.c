30c20b0a14ff744f64ac9d53694f2584592165d3
sallebruno <9392@holbertonstudents.com>
2024-06-24 20:55:04 +0000

    program 6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "length.c"
+/**
+ * print_chessboard - Prints a chessboard
+ * @a: Nested Array
+ * Return: None
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int v = 0;
+
+	while (i < 8)
+	{
+		while (v < 8)
+		{
+			_putchar(a[i][v]);
+			v++;
+		}
+		_putchar('\n');
+		v = 0;
+		i++;
+	}
+}
+~
