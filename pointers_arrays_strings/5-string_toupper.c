0af7a686ff76e05aef409bff83aeaa00606fa7c5
sallebruno <9392@holbertonstudents.com>
2024-06-24 20:27:21 +0000

    program 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - Changes all Lower to Upper in a String
+ * @str: String
+ * Return: String
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
