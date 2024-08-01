fd6e12518079b9a1cbf8bc505a43b56c310077f9
sallebruno <9392@holbertonstudents.com>
2024-08-01 18:42:05 -0300

    correct

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -4,27 +4,28 @@
 #include <stdlib.h>
 
 /**
- * struct list_s - doubly linked list
+ * struct dlistint_s - doubly linked list
  * @n: integer
- * @prev: pointer to the previous node
- * @next: pointer to the next node
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
  * Description: doubly linked list node structure
- */
-typedef struct list_s
+*/
+typedef struct dlistint_s
 {
-  int n;
-  struct list_s *prev;
-  struct list_s *next;
-} list_t;
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const int n);
-list_t *add_node_end(list_t **head, const int n);
-void free_list(list_t *head);
-list_t *get_node_at_index(list_t *head, unsigned int index);
-int sum_list(list_t *head);
-list_t *insert_node_at_index(list_t **head, unsigned int idx, int n);
-int delete_node_at_index(list_t **head, unsigned int index);
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-#endif
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif /* LISTS_H */
