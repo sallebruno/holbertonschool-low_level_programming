a6d14a3bd7b280827e345fe95edf7fa0311cf035
sallebruno <9392@holbertonstudents.com>
2024-08-01 18:19:50 -0300

    betty correct

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t count = 0;
+    size_t count = 0;
 
-  while (h)
-  {
-    count++;
+    while (h)
+    {
+        count++;
     printf("%d\n", h->n);
     h = h->next;
-  }
+    }
 
-  return (count);
+    return (count);
 }
