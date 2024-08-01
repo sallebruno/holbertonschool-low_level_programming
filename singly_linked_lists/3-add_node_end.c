cda5fdd6fe4935aef5e11e14fa1cb602fe1e9788
sallebruno <9392@holbertonstudents.com>
2024-08-01 15:24:40 +0000

    four exercise

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *current;
+	char *dup;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = dup;
+	new->len = strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new;
+	}
+	return (new);
+}
