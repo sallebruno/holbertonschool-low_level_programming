6153f10946035595a90bb722fb453d213aa4c01b
sallebruno <9392@holbertonstudents.com>
2024-08-02 10:40:23 -0300

    update

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * dlistint_len - returns the number of elements in the linked dlistint_t list.
  * @h: pointer to the first node of the list
  *
  * Return: the number of nodes in the list
