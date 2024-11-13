ec8643cb80b56433aae8408a3e5061e41cc8f656
sallebruno <9392@holbertonstudents.com>
2024-11-13 16:41:29 +0000

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  * @size: length of array
  * @c: c
  * Return: if size = 0 or the function fails - NULL.
- * 	Otherwise - a pointer to the array.
+ *		Otherwise - a pointer to the array.
  */
 
 char *create_array(unsigned int size, char c)
@@ -18,15 +18,15 @@ char *create_array(unsigned int size, char c)
 	array = malloc(size * sizeof(char));
 	if (size == 0)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	if (array == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
-	return(array);
+	return (array);
 }
