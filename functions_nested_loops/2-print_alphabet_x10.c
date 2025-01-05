8db4567f08fa10d380838fdf0cdb0d57f385d9cb
sallebruno <9392@holbertonstudents.com>
2025-01-05 21:42:36 +0000

    task 1,2,3

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, 
+ *				in lowercase, followed by a new line.
+ */
+
+void print_alphabet_x10(void)
+{
+	int repeat;
+	char letter;
+
+	for (repeat = 1; repeat <= 10; repeat++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
