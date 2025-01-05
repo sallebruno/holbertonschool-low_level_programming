662f99c76282fc5cbea057f0c076757b8ce97514
sallebruno <9392@holbertonstudents.com>
2025-01-05 21:49:28 +0000

    task 1,2,3

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, 
+ * print_alphabet_x10 - Prints 10 times the alphabet,
  *				in lowercase, followed by a new line.
  */
 
