5c99c2f132ff854d2f0bb99534a1ba75e05da7d7
sallebruno <9392@holbertonstudents.com>
2025-01-11 21:19:29 +0000

    .h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,12 +18,37 @@ struct dog
 /**
  * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: A dog struct
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - Prints the details of a dog
+ * @d: A dog struct
+ */
 void print_dog(struct dog *d);
+
+/**
+ * new_dog - Creates a new dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ * 
+ * Return: A pointer to the new dog struct
+ */
 dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * free_dog - Frees a dog
+ * @d: A pointer to the dog to be freed
+ */
 void free_dog(dog_t *d);
 
 #endif
