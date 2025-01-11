c6648dd4a2b80f3f2b2ed601a82ccf5ea7f08d04
sallebruno <9392@holbertonstudents.com>
2025-01-11 01:58:21 +0000

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,25 +8,26 @@
  * Return: The natural square root of n, or -1 if it doesn't exist.
  */
 
- int _sqrt_helper(int n, int guess)
- {
+int _sqrt_helper(int n, int guess)
+{
 	if (guess * guess == n)
-	    return guess;
+		return (guess);
 	if (guess * guess > n)
-	    return -1;
-	return _sqrt_helper(n, guess + 1);
- }
+		return (-1);
+	return (_sqrt_helper(n, guess + 1));
+}
 
- /**
-  * _sqrt_recursion - Returns the natural square root of a number.
-  *
-  * @n: The number to find the square root of.
-  * Return: The natural square root of n, or -1 if it doesn't exist.
-  */
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ *
+ * @n: The number to find the square root of.
+ * Return: The natural square root of n, or -1 if it doesn't exist.
+ */
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-        return -1;
-    return _sqrt_helper(n, 0);
+		return (-1);
+
+	return (_sqrt_helper(n, 0));
 }
