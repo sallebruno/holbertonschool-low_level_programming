ab6843db55edfe41c7067e7bb0671e6215f1d38d
sallebruno <9392@holbertonstudents.com>
2025-01-11 15:40:39 +0000

    task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	{
 		for (digit = 0; argv[num][digit]; digit++)
 		{
-			if (argv[num][digit] < '0' || argv[num][digit] < '9')
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
 			{
 				printf("Error\n");
 				return (1);
